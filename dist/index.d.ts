77cc980684a4a4c6a4bba13d683cf62d068138b4
Vlad Nicula <vladnicula@gmail.com>
2020-08-31 12:25:22 +0300

    Allow dist folder to be shared for now

---
+++ dist/index.d.ts
@@ -0,0 +1,79 @@
+declare type ObjectTree = object;
+export declare const Patcher: unique symbol;
+declare type JSONPatch = {
+    op: 'replace' | 'remove' | 'add';
+    path: string;
+    value: unknown;
+    old?: unknown;
+};
+export declare type JSONPatchEnhanced = JSONPatch & {
+    pathArray: string[];
+};
+/**
+ * Was used to apply changes in the mutation function after all the operatoins finished.
+ * I changed that to allow writing immediatly in the mutation. Now, when a class instance
+ * makes a change somewhere deep in the tree, the change happens immedtialy. I keep track
+ * of it in the json patch operations and can reason about it later on.
+ *
+ * This will come in handy for real time colaboraiton when changes from the server will be
+ * captured and handled by clients.
+ */
+export declare const applyInternalMutation: <T extends object>(mutations: JSONPatchEnhanced[], stateTree: T) => void;
+export declare const combinedJSONPatches: (operations: JSONPatchEnhanced[]) => JSONPatchEnhanced[];
+export declare const applyJSONPatchOperation: <T extends object>(operation: JSONPatchEnhanced, stateTree: T) => void;
+export declare const mutateFromPatches: <T extends object>(stateTree: T, patches: JSONPatchEnhanced[]) => void;
+export declare const mutate: <T extends object>(stateTree: T, callback: (mutable: T) => unknown) => JSONPatchEnhanced[];
+/**
+ * When working with domain objects, it's probably best to have a
+ * method that serializes them so we can 'snapshot' how they origianlly
+ * looked like before a changed appened. Without this, object spreads
+ * on those object might not create the best results.
+ *
+ * For the first phase of this, I'm only looking at plain objects in
+ * the initial algorithm. In the second phase this might come in handy.
+ */
+export declare abstract class IObservableDomain {
+    abstract toJSON: () => Record<string, unknown>;
+    abstract fromJSON: (input: Record<string, unknown>) => void;
+}
+export declare class ProxyMutationObjectHandler<T extends object> {
+    readonly pathArray: string[];
+    readonly deleted: Record<string, boolean>;
+    readonly original: Partial<T>;
+    readonly ops: JSONPatch[];
+    constructor(pathArray: string[]);
+    get<K extends keyof T>(target: T, prop: K): any;
+    set<K extends keyof T>(target: T, prop: K, value: T[K]): boolean;
+    /**
+     * Proxy trap for delete keyword
+     */
+    deleteProperty<K extends keyof T>(target: T, prop: K): boolean;
+    /**
+     * Proxy trap for Object.getOwnPropertyDescriptor()
+     */
+    getOwnPropertyDescriptor<K extends keyof T>(target: T, prop: K): PropertyDescriptor | undefined;
+    /**
+     * Proxy trap for when looking at what keys we have
+     */
+    ownKeys(target: T): (string | number | symbol)[];
+    /**
+     * Proxy trap for when looking at what keys we have
+     */
+    has<K extends keyof T>(target: T, key: K): boolean;
+}
+export declare const observe: <T extends object>(stateTree: T, selector: (selectableState: T) => unknown) => void;
+export declare const pathMatchesSource: (source: string[], target: string[]) => boolean;
+declare class StateTreeSelector<T extends ObjectTree, MP extends SeletorMappingBase<T>> {
+    private selectorSet;
+    private mappingFn;
+    private callbackSet;
+    private disposeMethod;
+    constructor(selectorSet: string[], mappingFn: MP, disposeMethod: Function);
+    match(pathArrays: string[][]): false | string[];
+    run(stateTree: T, pathsArray: JSONPatchEnhanced[]): void;
+    observe(callback: (input: ReturnType<MP>) => unknown): void;
+    dispose(): void;
+}
+declare type SeletorMappingBase<T> = (s: T, patches: JSONPatchEnhanced[]) => unknown;
+export declare const select: <T extends object, MP extends SeletorMappingBase<T>>(stateTree: T, selectors: string[], mappingFn: MP) => StateTreeSelector<T, MP>;
+export {};
