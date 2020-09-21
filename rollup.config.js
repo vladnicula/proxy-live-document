b48f83a1b5a763b4f9dc09f18c5374b869800205
Jaya Krishna Namburu <namburu1995@gmail.com>
2020-09-21 02:54:20 -0400

    Setup/build (#5)
    * Enabling workflows and cleanup for builds
    
    * Removed umd build

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,22 @@
-// todo checkout https://github.com/alexjoverm/typescript-library-starter/blob/master/package.json
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
-import typescript from 'rollup-plugin-typescript2'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import {terser} from "rollup-plugin-terser"
-
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
   input: `src/index.ts`,
   watch: {
-    include: 'src/**',
+    include: "src/**",
   },
-  plugins: [
-    typescript(),
-    peerDepsExternal({
-      packageJsonPath: './package.json'
-    }),
-    terser(),
-  ],
+  plugins: [typescript(), terser()],
   output: [
     {
       file: pkg.main,
-      format: 'cjs'
+      format: "cjs",
     },
     {
       file: pkg.module,
-      format: 'es' // the preferred format
+      format: "es",
     },
-    // {
-    //   file: pkg.browser,
-    //   format: 'iife',
-    //   name: 'PLD' // the global which can be used in a browser
-    // }
   ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ]
-}
+};
